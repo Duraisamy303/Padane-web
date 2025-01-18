a5c31726382028ea1922eae2f642953c3a63ba6d
Alaguraj1 <alaguraj259@gmail.com>
2025-01-18 12:02:42 +0530

    changes for chatbox screen

--- app/chatbox/components/chat-box.tsx
+++ app/chatbox/components/chat-box.tsx
@@ -1,18 +1,54 @@
 "use client"; // Mark this file as a client-side component
 
 import React, { useState, useRef, useEffect } from "react";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { Avatar } from "@radix-ui/react-avatar";
-import { ChevronLeftIcon } from "@radix-ui/react-icons";
-import { SendIcon, X } from "lucide-react";
+import {
+  ChevronLeftIcon,
+  DotFilledIcon,
+  DotsHorizontalIcon,
+  ExclamationTriangleIcon,
+  QuestionMarkCircledIcon,
+} from "@radix-ui/react-icons";
+import {
+  LocateIcon,
+  LogOutIcon,
+  SendIcon,
+  SettingsIcon,
+  X,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const ChatOption = () => {
   const [selectedChat, setSelectedChat] = useState<number | null>(null);
   const [message, setMessage] = useState("");
   const [searchTerm, setSearchTerm] = useState(""); // State for the search term
-  const [chats, setChats] = useState([
+  const [chats, setChats] = useState<any>([
     {
       id: 1,
       name: "John Doe",
@@ -114,8 +150,52 @@ const ChatOption = () => {
     <div className="flex max-w-full  flex-col overflow-hidden bg-gray-100 lg:flex-row">
       {/* Left side: Chat List */}
       <div
-        className={`mb-4 ml-4 mr-4 mt-4 h-[85vh] w-full overflow-y-auto rounded-lg bg-white p-4 shadow-md lg:mr-0 lg:block xl:w-1/4 lg:w-2/5 ${drawerOpen ? "block" : "hidden"} lg:visible`}
+        className={`mb-4 ml-4 mr-4 mt-4 h-[85vh] w-full overflow-y-auto rounded-lg bg-white p-4 shadow-md lg:mr-0 lg:block lg:w-2/6 xl:w-1/4 2xl:w-1/6 ${drawerOpen ? "block" : "hidden"} lg:visible`}
       >
+        <div
+          className={cn(
+            "flex cursor-pointer items-center space-x-4 rounded-md p-3",
+          )}
+        >
+          <Avatar className="h-10 w-10 overflow-hidden rounded-full">
+            <img
+              src={`https://i.pravatar.cc/150?img=${chats[0].id}`}
+              alt={`${chats[0].name}'s avatar`}
+            />
+          </Avatar>
+          <div className="flex-grow">
+            <p className="text-md font-medium">{chats[0].name}</p>
+            <p className="text-sm text-gray-500">Band</p>
+          </div>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <div className="rounded-full bg-gray-100 p-2">
+                <DotsHorizontalIcon className="h-5 w-5 text-gray-700" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40" align="end">
+              {/* Settings Item with Icon */}
+              <DropdownMenuItem>
+                <SettingsIcon className="mr-2 h-4 w-4 text-gray-500" />
+                Settings
+              </DropdownMenuItem>
+
+              {/* Help & Feedback Item with Icon */}
+              <DropdownMenuItem>
+                <QuestionMarkCircledIcon className="mr-2 h-4 w-4 text-gray-500" />
+                Help & feedback
+              </DropdownMenuItem>
+
+              {/* Sign Out Item with Icon */}
+              <DropdownMenuItem>
+                <LogOutIcon className="mr-2 h-4 w-4 text-gray-500" />
+                Sign Out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+
         {/* Global Search */}
         <div className="w-full p-4">
           <Input
@@ -135,16 +215,18 @@ const ChatOption = () => {
                 selectedChat === chat.id ? "bg-gray-200" : "",
               )}
             >
-              <Avatar className="h-12 w-12 overflow-hidden rounded-full">
+              <Avatar className="h-10 w-10 overflow-hidden rounded-full">
                 <img
                   src={`https://i.pravatar.cc/150?img=${chat.id}`}
                   alt={`${chat.name}'s avatar`}
                 />
               </Avatar>
               <div className="flex-grow">
-                <p className="text-lg font-medium">{chat.name}</p>
-                <p className="text-sm text-gray-500">
-                  {chat.messages[chat.messages.length - 1].text}
+                <p className="text-md font-medium">{chat.name}</p>
+                <p className="text-xs text-gray-500">
+                  {chat.messages[chat.messages.length - 1].text.length > 20
+                    ? `${chat.messages[chat.messages.length - 1].text.slice(0, 20)}...`
+                    : chat.messages[chat.messages.length - 1].text}
                 </p>
               </div>
               <p className="text-xs text-black">
@@ -158,7 +240,7 @@ const ChatOption = () => {
       </div>
 
       {/* Right side: Chat Window */}
-      <div className="mb-4 relative ml-4 mr-4 mt-4 flex h-[85vh] w-[93%] flex-col rounded-lg bg-white p-4 shadow-md md:w-[95%] xl:w-3/4 lg:w-3/5">
+      <div className="relative mb-4 ml-4 mr-4 mt-4 flex h-[85vh] w-[93%] flex-col rounded-lg bg-white p-4 shadow-md md:w-[95%] lg:w-4/6 xl:w-3/4 2xl:w-5/6">
         {/* User Profile (Sticky at top) */}
         {selectedChat !== null ? (
           <div className="sticky top-0  bg-gray-100 p-4 shadow-md">
@@ -173,12 +255,86 @@ const ChatOption = () => {
                 </Button>
               </div>
 
-              <Avatar className="h-12 w-12 overflow-hidden rounded-full">
-                <img
-                  src={`https://i.pravatar.cc/150?img=${selectedChat}`}
-                  alt={`User profile`}
-                />
-              </Avatar>
+              <Dialog>
+                <DialogTrigger asChild>
+                  {/* Wrap Avatar and the profile details in a parent div */}
+                  <div className="flex items-center space-x-4 ">
+                    <Avatar className="h-12 w-12 cursor-pointer overflow-hidden rounded-full">
+                      <img
+                        src={`https://i.pravatar.cc/150?img=${selectedChat}`}
+                        alt={`User profile`}
+                      />
+                    </Avatar>
+                  </div>
+                </DialogTrigger>
+
+                <DialogContent className="sm:max-w-[350px] md:max-w-[475px]">
+                  <DialogHeader>
+                    <DialogTitle>Profile Info</DialogTitle>
+                    {/* <DialogDescription>
+                      Make changes to your profile here. Click save when you're
+                      done.
+                    </DialogDescription> */}
+                  </DialogHeader>
+                  <div className="mt-2 flex flex-col items-center text-center">
+                    <Avatar className="h-30 w-30 mb-4 overflow-hidden rounded-full">
+                      <img
+                        src={`https://i.pravatar.cc/150?img=${selectedChat}`}
+                        alt={`User profile`}
+                      />
+                    </Avatar>
+                    <p className="text-xl font-semibold">
+                      {chats.find((chat) => chat.id === selectedChat)?.name}
+                    </p>
+                    <p className="text-sm text-gray-500">Band</p>
+                    {/* <p className="mt-4 text-gray-400">
+                      Additional information or details can go here.
+                    </p> */}
+                  </div>
+                  <div className="mt-2 flex space-x-3 text-gray-500">
+                    <p>INFORMATION</p>
+                  </div>
+                  {/* <DialogFooter>
+                    <Button type="submit">Save changes</Button>
+                  </DialogFooter> */}
+
+                  <Card className="w-full">
+                    <CardContent className="p-4">
+                      <div className="flex items-center gap-3">
+                        {" "}
+                        {/* Use Tailwind's flex utilities */}
+                        <div className="flex-shrink-0">
+                          {" "}
+                          {/* Prevent the icon from shrinking */}
+                          <LocateIcon className="h-6 w-6" />
+                        </div>
+                        <div>
+                          <p className="text-sm font-medium">Location</p>
+                          <p className="text-sm text-gray-500">Coimbatore</p>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                  <Card className="w-full">
+                    <CardContent className="p-4">
+                      <div className="flex items-center gap-3">
+                        {" "}
+                        {/* Use Tailwind's flex utilities */}
+                        <div className="flex-shrink-0">
+                          {" "}
+                          {/* Prevent the icon from shrinking */}
+                          <ExclamationTriangleIcon className="h-6 w-6" />
+                        </div>
+                        <div>
+                          <p className="text-sm font-medium">About Me</p>
+                          <p className="text-sm text-gray-500">Testing</p>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </DialogContent>
+              </Dialog>
+
               <div className="flex flex-col">
                 <p className="text-lg font-medium">
                   {chats.find((chat) => chat.id === selectedChat)?.name}
@@ -293,8 +449,52 @@ const ChatOption = () => {
         className={`fixed inset-0 bg-gray-800 bg-opacity-50 transition-opacity lg:hidden ${drawerOpen ? "opacity-100" : "pointer-events-none opacity-0 "}`}
       >
         <div className="fixed left-0 top-0 h-full w-full overflow-y-auto bg-white p-4">
-          <div className="flex items-center justify-end">
-            <X className="h-8 w-8 " onClick={() => setDrawerOpen(false)} />
+          <div
+            className={cn(
+              "flex cursor-pointer items-center space-x-4 rounded-md p-3",
+            )}
+          >
+            <Avatar className="h-10 w-10 overflow-hidden rounded-full">
+              <img
+                src={`https://i.pravatar.cc/150?img=${chats[0].id}`}
+                alt={`${chats[0].name}'s avatar`}
+              />
+            </Avatar>
+            <div className="flex-grow">
+              <p className="text-md font-medium">{chats[0].name}</p>
+              <p className="text-sm text-gray-500">Band</p>
+            </div>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <div className="rounded-full bg-gray-100 p-2">
+                  <DotsHorizontalIcon className="h-5 w-5 text-gray-700" />
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-40" align="end">
+                {/* Settings Item with Icon */}
+                <DropdownMenuItem>
+                  <SettingsIcon className="mr-2 h-4 w-4 text-gray-500" />
+                  Settings
+                </DropdownMenuItem>
+
+                {/* Help & Feedback Item with Icon */}
+                <DropdownMenuItem>
+                  <QuestionMarkCircledIcon className="mr-2 h-4 w-4 text-gray-500" />
+                  Help & feedback
+                </DropdownMenuItem>
+
+                {/* Sign Out Item with Icon */}
+                <DropdownMenuItem>
+                  <LogOutIcon className="mr-2 h-4 w-4 text-gray-500" />
+                  Sign Out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <div className="flex items-center justify-end">
+              <X className="h-5 w-5" onClick={() => setDrawerOpen(false)} />
+            </div>
           </div>
 
           {/* Global Search */}
@@ -316,15 +516,15 @@ const ChatOption = () => {
                   selectedChat === chat.id ? "bg-gray-200" : "",
                 )}
               >
-                <Avatar className="h-12 w-12 overflow-hidden rounded-full">
+                <Avatar className="h-10 w-10 overflow-hidden rounded-full">
                   <img
                     src={`https://i.pravatar.cc/150?img=${chat.id}`}
                     alt={`${chat.name}'s avatar`}
                   />
                 </Avatar>
                 <div className="flex-grow">
-                  <p className="text-lg font-medium">{chat.name}</p>
-                  <p className="text-sm text-gray-500">
+                  <p className="text-md font-medium">{chat.name}</p>
+                  <p className="text-xs text-gray-500">
                     {chat.messages[chat.messages.length - 1].text}
                   </p>
                 </div>
