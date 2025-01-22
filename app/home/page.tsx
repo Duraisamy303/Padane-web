1491d5b71ff0a00da5a956a4627bf71a40aabf32
Duraisamy <duraisamy.p@sanorac.com>
2025-01-22 16:20:02 +0530

    before login home screen updated

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -139,7 +139,7 @@ export default function Home() {
   ];
 
   return (
-    <div className="md:p-20 container">
+    <div className="container md:p-20">
       {/* Top Section */}
       <div className="flex w-full flex-col items-center md:flex-row md:space-x-8">
         <div className="mb-8 flex w-full items-center justify-center md:w-1/2">
@@ -170,7 +170,7 @@ export default function Home() {
       <div className="md:pt-10">
         <h1 className="mb-4 text-[30px] font-bold md:text-[30px]">
           Events near
-        </h1> 
+        </h1>
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {nearEvent.map((item, index) => (
             <Event_cart key={index} item={item} />
@@ -189,7 +189,6 @@ export default function Home() {
           ))}
         </div>
       </div> */}
-    
 
       <div className="md:pt-10">
         <h1 className="mb-4 text-[30px] font-bold md:text-[30px]">
@@ -203,7 +202,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="md:pt-10">
+      <div className="pt-5 ">
         <h1 className="mb-4 text-[30px] font-bold md:text-[30px]">
           Explore top categories
         </h1>
@@ -215,7 +214,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="md:pt-10">
+      <div className="pt-5 ">
         <h1 className="mb-4 text-[30px] font-bold md:text-[30px]">
           How Meetup works
         </h1>
